e63f32aa5996b70c1310b44b4522fd9f04efb561
suresh-03 <sureshkumar732003@gmail.com>
2025-05-06 18:32:14 +0530

    project setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace e_commerce_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
