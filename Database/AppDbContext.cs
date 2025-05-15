7f59a0713c9a4d942aea73f65fe88cda65e6930d
Suresh-03 <sureshkumar732003@gmail.com>
2025-05-15 15:54:03 +0530

    DbContext added

---
+++ Database/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using e_commerce_website.Models;
+using Microsoft.EntityFrameworkCore;
+
+   
+
+    namespace e_commerce_website.Database
+    {
+        public class AppDbContext : DbContext
+        {
+            // Constructor
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+            // DbSets for your tables
+            public DbSet<User> Users { get; set; }
+            public DbSet<Role> Roles { get; set; }
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Review> Reviews { get; set; }
+            public DbSet<Category> Categories { get; set; }
+            public DbSet<Brand> Brands { get; set; }
+            public DbSet<ProductVariant> ProductVariants { get; set; }
+            public DbSet<ProductImage> ProductImages { get; set; }
+            public DbSet<Wishlist> Wishlists { get; set; }
+            public DbSet<Cart> Carts { get; set; }
+            public DbSet<Order> Orders { get; set; }
+            public DbSet<OrderItem> OrderItems { get; set; }
+            public DbSet<Payment> Payments { get; set; }
+        }
+    }
+
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using e_commerce_website.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
